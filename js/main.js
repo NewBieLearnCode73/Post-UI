82993e12acc0c82e7f3d492aa8445444ae637810
DinhChieu_73 <ndchieu73@gmail.com>
2024-07-28 09:35:05 +0700

    Add api method

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log('Hello main.js');
+import axios from 'axios';
+import axiosClient from './axiosClient';
+import postApi from './postApi';
+
+async function main() {
+  try {
+    const queryParamss = {
+      _page: 1,
+      _limit: 5,
+    };
+
+    const data = await postApi.getAll(queryParamss);
+  } catch (error) {
+    console.log(error);
+  }
+
+  await postApi.updateFormData({
+    id: 'sktwi1cgkkuif36do',
+    title: 'Adipisci a enim 444',
+  });
+}
+
+main();
